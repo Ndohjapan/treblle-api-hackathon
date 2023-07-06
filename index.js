4c417e6f8c1d0b4a593d71f7e75e4b2e52cc7f5f
Jau Stance <jaystance25@gmail.com>
2023-07-06 11:43:54 +0100

    feat: schedule unused session deletion

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const { app } = require("./src/app");
 const { connectToDatabase } = require("./src/database/connection");
+const { SessionService } = require("./src/service/session-service");
+const session = new SessionService();
 require("dotenv").config();
 
 // eslint-disable-next-line no-undef
@@ -7,7 +9,9 @@ const PORT = process.env.PORT || 7001;
 
 // Connect to database
 connectToDatabase();
+// Delete unused sessions
+session.sheduleSessionDelete();
 
-app.listen(PORT, async () => {
-  console.log("Server is running on port " + PORT);
+app.listen(PORT, async() => {
+    console.log("Server is running on port " + PORT);
 });
