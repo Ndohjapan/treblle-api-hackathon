2a224b2ce088af966c91a96053c858833f4e2efe
Jau Stance <jaystance25@gmail.com>
2023-07-06 08:07:24 +0100

    feat: created the filter and find by id messages

--- src/middleware/protect.js
+++ src/middleware/protect.js
@@ -5,29 +5,26 @@ const jwtConfig = config.get("jwt");
 const jwt = require("jsonwebtoken");
 
 const userAuth = async(req, res, next) => {
-
-  if (req.isAuthenticated()) {
-    const bearerToken = req.headers.authorization;
-    const token = bearerToken.split(" ")[1];
-    try {
-      jwt.verify(token, jwtConfig.secret); 
-      next();
-    } catch (error) {
-      return next(new AuthException(en.authentication_failure));
-        
+    if (req.isAuthenticated()) {
+        try {
+            const bearerToken = req.headers.authorization;
+            const token = bearerToken.split(" ")[1];
+            jwt.verify(token, jwtConfig.secret);
+            next();
+        } catch (error) {
+            return next(new AuthException(en.authentication_failure));
+        }
+    } else {
+        return next(new AuthException(en.authentication_failure));
     }
-  } else {
-    return next(new AuthException(en.authentication_failure));
-  }
 };
 
 const isAdmin = async(req, res, next) => {
-  if(req.user.isAdmin){
-    next();
-  }
-  else{
-    return next(new AuthException(en.authentication_forbidden, 403));
-  }
+    if (req.user.isAdmin) {
+        next();
+    } else {
+        return next(new AuthException(en.authentication_forbidden, 403));
+    }
 };
 
-module.exports = {userAuth, isAdmin};
+module.exports = { userAuth, isAdmin };
--- src/service/message-service.js
+++ src/service/message-service.js
@@ -3,62 +3,65 @@ const NotFoundException = require("../error/not-found-exception");
 const CreationException = require("../error/creation-exception");
 const MessageRepository = require("../database/repository/message-repository");
 
-
 class MessageService {
-  constructor() {
-    this.repository = new MessageRepository();
-  }
-
-  async CreateMessage(data) {
-    try {
-      const message = await this.repository.CreateMessage(data);
-
-      return message;
-    } catch (error) {
-      throw new CreationException(en.message_creation_error, 401);
+    constructor() {
+        this.repository = new MessageRepository();
     }
-  }
 
-  async FindById(id) {
-    try {
-      const message = await this.repository.FindUserConnectionById(id);
+    async CreateMessage(data) {
+        try {
+            const message = await this.repository.CreateMessage(data);
 
-      return message;
-    } catch (error) {
-      throw new NotFoundException(en.message_find_error);
+            return message;
+        } catch (error) {
+            throw new CreationException(en.message_creation_error, 401);
+        }
     }
-  }
 
-  async FilterMessage({ page, limit, data }) {
-    let updateData = {};
+    async FindById(id) {
+        try {
+            const message = await this.repository.FindMessageById(id);
 
-    Object.entries(data).forEach(([key, value]) => {
-      if (value != "") {
-        updateData[key] = value;
-      }
-    });
-
-    try {
-      const messages = await this.repository.FilterMessage({
-        page,
-        limit,
-        data: updateData,
-      });
-      return messages;
-    } catch (error) {
-      throw new NotFoundException(en.message_find_error);
+            return message;
+        } catch (error) {
+            throw new NotFoundException(en.message_find_error);
+        }
     }
-  }
 
-  async DeleteMessage(id) {
-    try {
-      const message = await this.repository.DeleteOne(id);
+    async FilterMessage({ page, limit, data }) {
+        let updateData = {};
 
-      return message;
-    } catch (error) {
-      throw new NotFoundException(en.message_delete_error);
+        Object.entries(data).forEach(([key, value]) => {
+            if (value != "") {
+                updateData[key] = value;
+            }
+        });
+
+        try {
+            const messages = await this.repository.FilterMessage({
+                page,
+                limit,
+                data: updateData,
+            });
+            return messages;
+        } catch (error) {
+            throw new NotFoundException(en.message_find_error);
+        }
     }
-  }
+
+    async DeleteMessage(id) {
+        try {
+            const isMessage = await this.repository.FindMessageById(id);
+            if (isMessage.id) {
+                const message = await this.repository.DeleteOne(id);
+
+                return message;
+            }
+            throw new Error(en.message_find_error);
+        } catch (error) {
+            throw new NotFoundException(error.message || en.message_delete_error);
+        }
+    }
 }
 
 module.exports = { MessageService };
